8f3cf71cfe3e40ee7840c33b744987b49f062bed
gang <ganggstand@gmail.com>
2023-02-13 11:37:59 +0300

    add post pokemon

--- API/Interfaces/IPokemonRepository.cs
+++ API/Interfaces/IPokemonRepository.cs
@@ -1,12 +1,16 @@
-﻿using API.Models;
+﻿using API.Data.Dto;
+using API.Models;
 
 namespace API.Interfaces;
 
 public interface IPokemonRepository
 {
-    List<Pokemon> GetPokemons();
+    List<Pokemon?> GetPokemons();
     Pokemon? GetPokemon(int id);
     Pokemon? GetPokemon(string name);
+    Pokemon GetPokemonTrimToUpper(PokemonDto pokemonCreate);
     decimal GetPokemonRating(int pokeId);
     bool PokemonExists(int pokeId);
+    bool CreatePokemon(int ownerId, int categoryId, Pokemon pokemon);
+    bool Save();
 }
--- API/Repository/PokemonRepository.cs
+++ API/Repository/PokemonRepository.cs
@@ -1,4 +1,5 @@
 ﻿using API.Data;
+using API.Data.Dto;
 using API.Interfaces;
 using API.Models;
 
@@ -13,9 +14,9 @@ public class PokemonRepository : IPokemonRepository
         _context = context;
     }
 
-    public List<Pokemon> GetPokemons()
+    public List<Pokemon?> GetPokemons()
     {
-        return _context.Pokemon.OrderBy(p => p!.Id).ToList();
+        return _context.Pokemon.OrderBy(p => p!.Id).ToList()!;
     }
 
     public Pokemon? GetPokemon(int id)
@@ -36,5 +37,41 @@ public class PokemonRepository : IPokemonRepository
     public bool PokemonExists(int pokeId)
     {
         return _context.Pokemon.Any(p => p!.Id == pokeId);
+    }
+
+    public bool CreatePokemon(int ownerId, int categoryId, Pokemon pokemon)
+    {
+        var pokemonOwnerEntity = _context.Owners.FirstOrDefault(a => a.Id == ownerId);
+        var category = _context.Categories.FirstOrDefault(a => a.Id == categoryId);
+        var pokemonOwner = new PokemonOwner()
+        {
+            Owner = pokemonOwnerEntity,
+            Pokemon = pokemon,
+        };
+
+        _context.Add(pokemonOwner);
+
+        var pokemonCategory = new PokemonCategory()
+        {
+            Category = category,
+            Pokemon = pokemon,
+        };
+
+        _context.Add(pokemonCategory);
+
+        _context.Add(pokemon);
+
+        return Save();
+    }
+
+    public Pokemon GetPokemonTrimToUpper(PokemonDto pokemonCreate)
+    {
+        return GetPokemons().FirstOrDefault(c => String.Equals(c!.Name!.Trim(), pokemonCreate.Name!.TrimEnd(), StringComparison.CurrentCultureIgnoreCase))!;
+    }
+
+    public bool Save()
+    {
+        var saved = _context.SaveChanges();
+        return saved > 0 ? true : false;
     }
 }
